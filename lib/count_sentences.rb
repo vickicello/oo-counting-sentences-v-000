febe580e2761118495757cf1b3eafa15417b27b3
Vicki Hamm <vicki.cello@gmail.com>
2018-06-25 15:12:01 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    split_self = self.split(/\.|\?|\!/)
+    value = 0
+    split_self.each do |array|
+      if !(array == "")
+        value += 1
+      end
+    end
+    value
+   end
 
-  end
 end
